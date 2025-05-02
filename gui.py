changed some titling, added scatter plot, added more performance metrics, fixed scaling

gui.py
@@ -27,32 +27,40 @@ x
 x = df.select_dtypes(include="object").describe()
 x
 
+"Data info"
+from io import StringIO
+buffer = StringIO()
+df.info(buf=buffer)
+st.code(buffer.getvalue())
+
 "### Missing value handling"
 "Percentage of missing values in each column"
 df.isnull().sum() / df.shape[0] * 100
 
 "#### Simple imputer with mean for certain columns"
-"LotFrontage,MasVnrArea,GarageCars"
 from sklearn.impute import SimpleImputer
 imput = SimpleImputer(strategy="mean")
 df[["LotFrontage","MasVnrArea"]] = imput.fit_transform(df[["LotFrontage","MasVnrArea"]])
 df[["GarageCars"]] = imput.fit_transform(df[["GarageCars"]])
+df[['LotFrontage','MasVnrArea','GarageCars']]
 
 "#### Simple imputer with mode for certain columns"
-"BsmtQual , BsmtCond , BsmtExposure , BsmtFinType1 , BsmtFinType2 , Electrical , GarageType , GarageFinish"
 imputer = SimpleImputer(strategy="most_frequent")
 df[["BsmtQual" , "BsmtCond" , "BsmtExposure" , "BsmtFinType1" , "BsmtFinType2" , "Electrical" , "GarageType" , "GarageFinish"]] = imputer.fit_transform(df[["BsmtQual" , "BsmtCond" , "BsmtExposure" , "BsmtFinType1" , "BsmtFinType2" , "Electrical" , "GarageType" , "GarageFinish"]])
+df[['BsmtQual' , 'BsmtCond' , 'BsmtExposure' , 'BsmtFinType1' , 'BsmtFinType2' , 'Electrical' , 'GarageType' , 'GarageFinish']]
 
 "Percentage of missing values in each column after imputing"
 df.isnull().sum() / df.shape[0] * 100
 
 "### Detect and handle duplicates"
-f"Number of duplicates: {df.duplicated().sum()}"
+df.drop_duplicates(inplace=True)
+st.success(f"Succesfully removed **{df.duplicated().sum()}** duplicates")
 
 num_col = df.select_dtypes(include="number")
 cat_col = df.select_dtypes(include="object")
 
 def detect_handling_outlier(col_name):
+    global df
     Q1 = df[col_name].quantile(0.25)
     Q3 = df[col_name].quantile(0.75)
 
@@ -60,8 +68,9 @@ def detect_handling_outlier(col_name):
 
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
-    mean_value = df[col_name].mean()
-    df[col_name] = np.where((df[col_name] < lower_bound) | (df[col_name] > upper_bound) , mean_value, df[col_name])
+    # mean_value = df[col_name].mean()
+    # df[col_name] = np.where((df[col_name] < lower_bound) | (df[col_name] > upper_bound) , mean_value, df[col_name])
+    df = df[(df[col_name] >= lower_bound) & (df[col_name] <= upper_bound)]
 
 
 def draw_plots(plotfunc, include_num=False, include_cat=False):        
@@ -90,30 +99,32 @@ def draw_plots(plotfunc, include_num=False, include_cat=False):
     
 "### Detecting and handling outliers"
 "Before handling outlier"
+before_outlier_count = len(df)
 draw_plots(sns.boxplot, True, False)
-    
+
 "After handling outlier for features"
 for c in num_col:
     detect_handling_outlier(c)
+st.success(f"Succesfully removed **{before_outlier_count - len(df)}** outliers")
 draw_plots(sns.boxplot, True, False)
 
-"After handling outlier for labels"
-@st.cache_data
-def remove_outliers_iqr(dataframe, column):
-    Q1 = df[column].quantile(0.25)
-    Q3 = df[column].quantile(0.75)
+# "After handling outlier for labels"
+# @st.cache_data
+# def remove_outliers_iqr(dataframe, column):
+#     Q1 = df[column].quantile(0.25)
+#     Q3 = df[column].quantile(0.75)
 
-    IQR = Q3 - Q1
-    lower = Q1 - 1.5 * IQR
-    upper = Q3 + 1.5 * IQR
-    return df[(df[column] >= lower) & (df[column] <= upper)]
+#     IQR = Q3 - Q1
+#     lower = Q1 - 1.5 * IQR
+#     upper = Q3 + 1.5 * IQR
+#     return df[(df[column] >= lower) & (df[column] <= upper)]
 
 
-df = remove_outliers_iqr(df, "SalePrice")
-st.success("Handled succesfully")
+# df = remove_outliers_iqr(df, "SalePrice")
+# st.success("Handled succesfully")
 
 "### Visualization"
-heat_tab, bar_tab, hist_tab, count_tab = st.tabs(['Heat Map', 'Bar Plot', 'Histogram', 'Count Plot'])
+heat_tab, bar_tab, hist_tab, count_tab, scatter_tab = st.tabs(['Heat Map', 'Bar Plot', 'Histogram', 'Count Plot', 'Scatter Plot'])
 
 with heat_tab:
     st.snow()
@@ -129,36 +140,40 @@ with hist_tab:
 with count_tab:
     st.balloons()
     draw_plots(sns.countplot, False, True)
-    
+with scatter_tab:
+    draw_plots(sns.scatterplot, True, True)
+
 "### Label encoding categories"
 from sklearn.preprocessing import LabelEncoder
 encode = LabelEncoder()
 for c in cat_col:
     df[c] = encode.fit_transform(df[c])
 st.success("Succesfully Encoded features")
+df[cat_col.columns]
 
 "### Split data"
 x = df.drop("SalePrice", axis=1) 
 y = df["SalePrice"]
 st.success("Succesfully split into feature and label")
 
-num_col2 = x.select_dtypes(include="number")
-cat_col2 = x.select_dtypes(include="object")
+num_col = num_col.drop('SalePrice',axis=1)
 from sklearn.model_selection import train_test_split
 x_train , x_test , y_train , y_test = train_test_split(x,y,test_size=0.2,random_state=42) 
 st.success("Succesfully split into train and test data")
 
-"### Scaling data using minmax"
+"### Scaling features using minmax"
 from sklearn.preprocessing import MinMaxScaler
 min_max_scaler = MinMaxScaler()
-for c in num_col2:
+for c in num_col:
     x_train[[c]] = min_max_scaler.fit_transform(x_train[[c]])
     x_test[[c]] = min_max_scaler.fit_transform(x_test[[c]])
-st.success("Succesfully scaled feature data")
+st.success("Succesfully scaled **feature** data")
+x_train[num_col.columns]
 
 y_train_scaled = min_max_scaler.fit_transform(y_train.values.reshape(-1, 1))
 y_test_scaled = min_max_scaler.transform(y_test.values.reshape(-1, 1))
-st.success("Succesfully scaled label data")
+st.success("Succesfully scaled **label** data")
+y_train_scaled
 
 "### Model selection and training"
 "Using Random Forest Regressor"
@@ -171,6 +186,17 @@ st.success("Succesfully trained model")
 m_pridect = model.predict(x_test)
 st.success("Succesfully ran model prediction")
 
-"### Model Performance"
-from sklearn.metrics import r2_score
-st.success(f"Performace metric: **{r2_score(y_test,m_pridect)}**")
+"### Model Evaluation"
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from math import sqrt
+
+mse = mean_squared_error(y_test, m_pridect)
+rmse = sqrt(mse)
+mae = mean_absolute_error(y_test, m_pridect)
+r2 = r2_score(y_test, m_pridect)
+report = pd.DataFrame({
+    'Metric': ['Mean Squared Error', 'Root Mean Squared Error', 'Mean Absolute Error', 'R2 Score'],
+    'Value': [mse, rmse, mae, r2]
+})
+
+report